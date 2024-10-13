6731faab2aef4654616efbca0b08c5b43465a5fa
tiny <zaalire2@gmail.com>
2024-10-13 23:32:58 +0330

    make git better

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=511)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+
+class InCome(models.Model):
+    text = models.CharField(max_length=511)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
